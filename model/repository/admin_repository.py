6de87c0ce611ebaca572bc508337c57eed4b0ad0
user.py <roozbehbahrampoor18@gmail.com>
2025-07-24 21:45:38 +0330

    roozbeh change

--- model/repository/admin_repository.py
+++ model/repository/admin_repository.py
@@ -17,7 +17,8 @@ class AdminRepository:
     def edit(self, admin):
         connection = sqlite3.connect('store.db.sqlite')
         cursor = connection.cursor()
-        cursor.execute("SQL", [data])
+        cursor.execute("update admins set name=?, family=?, email=?, username=?, password=?, locked=?where code=?",
+                       [admin.name, admin.family, admin.email, admin.username, admin.password, admin.locked, admin.code])
         connection.commit()
         cursor.close()
         connection.close()
@@ -25,7 +26,7 @@ class AdminRepository:
     def delete(self, code):
         connection = sqlite3.connect('store.db.sqlite')
         cursor = connection.cursor()
-        cursor.execute("SQL", [data])
+        cursor.execute("delete from admins where code = ?", [code])
         connection.commit()
         cursor.close()
         connection.close()
@@ -34,34 +35,34 @@ class AdminRepository:
     def find_all(self):
         connection = sqlite3.connect('store.db.sqlite')
         cursor = connection.cursor()
-        cursor.execute("SQL", [data])
+        cursor.execute("select * from admins")
         cursor.close()
         connection.close()
 
     def find_by_code(self, code):
         connection = sqlite3.connect('store.db.sqlite')
         cursor = connection.cursor()
-        cursor.execute("SQL", [data])
+        cursor.execute("select * from admins where code = ?", [code])
         cursor.close()
         connection.close()
 
     def find_by_name_family(self, name, family):
         connection = sqlite3.connect('store.db.sqlite')
         cursor = connection.cursor()
-        cursor.execute("SQL", [data])
+        cursor.execute("select * from admins where name like ? and like ?", [name+"%", family+"%"])
         cursor.close()
         connection.close()
 
     def find_by_username(self, username):
         connection = sqlite3.connect('store.db.sqlite')
         cursor = connection.cursor()
-        cursor.execute("SQL", [data])
+        cursor.execute("select * from admins where username = ?", [username])
         cursor.close()
         connection.close()
 
     def find_by_username_and_password(self, username, password):
         connection = sqlite3.connect('store.db.sqlite')
         cursor = connection.cursor()
-        cursor.execute("SQL", [data])
+        cursor.execute("select * from admins where username = ? and password = ?", [username, password])
         cursor.close()
         connection.close()
--- model/repository/db.sql
+++ model/repository/db.sql
@@ -1,4 +1,15 @@
+-- Save User
 insert into admins
     (code, name, family, email, username, password, locked)
 values
     (3, 'yousef', 'yousefi', 'yousef_sh','yousef456','yousef123', 0);
+-- Search - Find
+select *
+from admins
+where code = 1;
+
+-- Edit
+update admins set name=?, family=?, email=?, username=?, password=?, locked=?
+where code=?;
+-- Delete
+delete from admins where code =1;

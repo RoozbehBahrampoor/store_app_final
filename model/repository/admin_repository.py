0cfb7db50355dbd14494a804310b045052e59a74
user.py <roozbehbahrampoor18@gmail.com>
2025-07-24 22:18:13 +0330

    roozbeh change

--- model/repository/admin_repository.py
+++ model/repository/admin_repository.py
@@ -1,68 +1,59 @@
 import sqlite3
 
+
 class AdminRepository:
+    def connect(self):
+        self.connection = sqlite3.connect("store.db.sqlite")
+        self.cursor = self.connection.cursor()
+
+    def disconnect(self, commit=False):
+        if commit:
+            self.connection.commit()
+        self.cursor.close()
+        self.connection.close()
+
     def save(self, admin):
-        connection = sqlite3.connect('store.db.sqlite')
-        cursor = connection.cursor()
-        cursor.execute(
+        self.connect()
+        self.cursor.execute(
             """insert into admins
-                (code, name, family, email, username, password, locked)
-            values
-                (?,?,?,?,?,?,?)""",
+                   (code, name, family, email, username, password, locked)
+               values (?, ?, ?, ?, ?, ?, ?)""",
             [admin.code, admin.name, admin.family, admin.email, admin.username, admin.password, admin.locked])
-        connection.commit()
-        cursor.close()
-        connection.close()
+        self.disconnect(commit=True)
 
     def edit(self, admin):
-        connection = sqlite3.connect('store.db.sqlite')
-        cursor = connection.cursor()
-        cursor.execute("update admins set name=?, family=?, email=?, username=?, password=?, locked=?where code=?",
-                       [admin.name, admin.family, admin.email, admin.username, admin.password, admin.locked, admin.code])
-        connection.commit()
-        cursor.close()
-        connection.close()
+        self.connect()
+        self.cursor.execute("update admins set name=?, family=?, email=?, username=?, password=?, locked=?where code=?",
+                            [admin.name, admin.family, admin.email, admin.username, admin.password, admin.locked,
+                             admin.code])
+        self.disconnect(commit=True)
 
     def delete(self, code):
-        connection = sqlite3.connect('store.db.sqlite')
-        cursor = connection.cursor()
-        cursor.execute("delete from admins where code = ?", [code])
-        connection.commit()
-        cursor.close()
-        connection.close()
-
+        self.connect()
+        self.cursor.execute("delete from admins where code = ?", [code])
+        self.disconnect(commit=True)
 
     def find_all(self):
-        connection = sqlite3.connect('store.db.sqlite')
-        cursor = connection.cursor()
-        cursor.execute("select * from admins")
-        cursor.close()
-        connection.close()
+        self.connect()
+        self.cursor.execute("select * from admins")
+        self.disconnect()
 
     def find_by_code(self, code):
-        connection = sqlite3.connect('store.db.sqlite')
-        cursor = connection.cursor()
-        cursor.execute("select * from admins where code = ?", [code])
-        cursor.close()
-        connection.close()
+        self.connect()
+        self.cursor.execute("select * from admins where code = ?", [code])
+        self.disconnect()
 
     def find_by_name_family(self, name, family):
-        connection = sqlite3.connect('store.db.sqlite')
-        cursor = connection.cursor()
-        cursor.execute("select * from admins where name like ? and like ?", [name+"%", family+"%"])
-        cursor.close()
-        connection.close()
+        self.connect()
+        self.cursor.execute("select * from admins where name like ? and family like ?", [name + "%", family + "%"])
+        self.disconnect()
 
     def find_by_username(self, username):
-        connection = sqlite3.connect('store.db.sqlite')
-        cursor = connection.cursor()
-        cursor.execute("select * from admins where username = ?", [username])
-        cursor.close()
-        connection.close()
+        self.connect()
+        self.cursor.execute("select * from admins where username = ?", [username])
+        self.disconnect()
 
     def find_by_username_and_password(self, username, password):
-        connection = sqlite3.connect('store.db.sqlite')
-        cursor = connection.cursor()
-        cursor.execute("select * from admins where username = ? and password = ?", [username, password])
-        cursor.close()
-        connection.close()
+        self.connect()
+        self.cursor.execute("select * from admins where username = ? and password = ?", [username, password])
+        self.disconnect()

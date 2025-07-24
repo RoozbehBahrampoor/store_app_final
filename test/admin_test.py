21704f5114e911f2282bc8fd951e92fa2060bd84
user.py <roozbehbahrampoor18@gmail.com>
2025-07-25 01:19:11 +0330

    roozbeh change

--- test/admin_test.py
+++ test/admin_test.py
@@ -1,21 +1,25 @@
 from controller.admin_controller import AdminController
 from model.entity.admin import Admin
 from model.repository.admin_repository import AdminRepository
+
 #
 # admin_1 = Admin(1, "parsa", "hosseini", "alihosseini123", "ali456", "alii1234", 1)
 
 # print(admin_1)
-
+admin_controller = AdminController()
 # admin_repo = AdminRepository()
 
 # Test Passed
 # admin_repo.save(admin_1)
+# status, message = (admin_controller.save(2, "akbar", "ahmadi", "akbarahmadi45", "akbar_ah14", "alii1234", 1))
 
 # Test Passed
 # admin_repo.edit(admin_1)
+# status, message = (admin_controller.edit(2, "akbar", "ahmadi", "akbarahmadi45", "akbar_ah14", "alii1234", 1))
 
 # Test Passed
 # admin_repo.delete(1)
+# status, message = (admin_controller.delete(2))
 
 # Test Passed
 # print(admin_repo.find_all())
@@ -31,6 +35,3 @@ from model.repository.admin_repository import AdminRepository
 
 # Test Passed
 # print(admin_repo.find_by_username_and_password("ahmad222", "ahmad456"))
-
-admin_controller = AdminController()
-status, message = (admin_controller.save(2, "akbar", "ahmadi", "akbarahmadi45", "akbar_ah14", "alii1234", 1))

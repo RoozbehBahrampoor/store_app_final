37afbab3ab27095da1c5b65294c2f864e51422ed
user.py <roozbehbahrampoor18@gmail.com>
2025-07-24 16:54:09 +0330

    roozbeh change

--- model/repository/database_creator.py
+++ model/repository/database_creator.py
@@ -2,15 +2,11 @@ import sqlite3
 
 connection = sqlite3.connect("store.db.sqlite")
 
-
 # cursor = connection.cursor()
-
 
 # cursor.execute("SQL")
 
-
 # connection.commit()
-
 
 # cursor.close()
 connection.close()
